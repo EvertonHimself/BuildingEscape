e07fd28434fe81a54a57318861bd2d14d07982da
EvertonHimself <evertondeoliveira@gmail.com>
2022-08-01 21:33:26 -0300

    Refactored code.
    Refactoring a lot of code and removing comments.

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -16,15 +17,28 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
 private:
 	float Reach = 100.f;
 
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	void Grab();
+	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+
+	// Return the first Actor within reach with physics body.
+	FHitResult GetFirstPhysicsBodyInReach() const;
+
+	// Return the Line Trace End.
+	FVector GetThePlayersReach() const;
 };
